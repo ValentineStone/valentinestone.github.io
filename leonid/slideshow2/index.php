550786440a49604b6a264617370b4dcc5c0a6b63
ValentineStone <sirValentineStone@gmail.com>
2019-09-22 15:45:30 +0500

    .

--- leonid/slideshow2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	header("refresh:1;url=index2.php");
-?>
-<html>
-	<body>
-		<h1>FILE1</h1>
-	</body>
-</html>
--- leonid/slideshow2/index2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	header("refresh:1;url=index.php");
-?>
-<html>
-	<body>
-		<h1>FILE2</h1>
-	</body>
-</html>
