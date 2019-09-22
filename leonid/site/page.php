e915a351e26028b61425368b891ea3e8740bf054
ValentineStone <sirValentineStone@gmail.com>
2019-09-22 16:13:00 +0500

    .

--- leonid/site/page.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<body>
-			<h1> <?php echo $_GET['p']; ?> </h1>
-			<p style="font-size: 15pt;">
-				<?php echo nl2br( file_get_contents($_GET['p']. '.php') ); ?>
-			</p>
-			
-	</body>
-</html>
