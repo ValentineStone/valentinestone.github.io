61400291424f92018a68d2158022a1140621107b
Leonid Romanovsky <sirLeonidRomanovsky@gmail.com>
2020-06-23 16:39:51 +0300

    added info loading to docblog

--- docblog-build.js
+++ docblog-build.js
@@ -1,36 +1,42 @@
 'use strict';
 
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
 
-window.h = React.createElement;
-document.querySelector('div.description').textContent = new Date().toLocaleDateString('ru-RU', {
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: '2-digit'
-});
-window.jsonpRequestId = 0;
+function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
 
-function jsonp(uri) {
-  return new Promise(function (resolve, reject) {
-    var id = '_' + Math.round(10000 * Math.random());
-    var callbackName = 'jsonp_callback_' + window.jsonpRequestId++ + id;
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
 
-    window[callbackName] = function (data) {
-      delete window[callbackName];
-      var ele = document.getElementById(id);
-      ele.parentNode.removeChild(ele);
-      resolve(data);
-    };
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-    var src = uri + '&callback=' + callbackName;
-    var script = document.createElement('script');
-    script.src = src;
-    script.id = id;
-    script.addEventListener('error', reject);
-    (document.getElementsByTagName('head')[0] || document.body || document.documentElement).appendChild(script);
+var apikey = 'AIzaSyCzDr38ArpkrukqI0gAN-rAYvbTCmEVyJ8';
+var folder = '1yFMsHwOKYa7n9kGx_cgLavLtIGxZ-oR1';
+
+function niceDate() {
+  var date = _construct(Date, Array.prototype.slice.call(arguments)).toLocaleDateString('ru-RU', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    minute: 'numeric',
+    hour: 'numeric'
+  });
+
+  return date.charAt(0).toUpperCase() + date.substring(1);
+}
+
+function niceDate2() {
+  return _construct(Date, Array.prototype.slice.call(arguments)).toLocaleDateString('ru-RU', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    minute: 'numeric',
+    hour: 'numeric'
   });
 }
+
+document.querySelector('div.description').textContent = niceDate();
 
 var App = /*#__PURE__*/function (_React$Component) {
   _inheritsLoose(App, _React$Component);
@@ -48,7 +54,9 @@ var App = /*#__PURE__*/function (_React$Component) {
   _proto.componentDidMount = function componentDidMount() {
     var _this2 = this;
 
-    jsonp('https://www.googleapis.com/drive/v3/files?q=%271yFMsHwOKYa7n9kGx_cgLavLtIGxZ-oR1%27+in+parents&key=AIzaSyCzDr38ArpkrukqI0gAN-rAYvbTCmEVyJ8').then(function (r) {
+    fetch("https://www.googleapis.com/drive/v3/files?q=%27" + folder + "%27+in+parents&key=" + apikey).then(function (r) {
+      return r.json();
+    }).then(function (r) {
       r.files.sort(function (f1, f2) {
         return f1.name < f2.name ? 1 : -1;
       });
@@ -59,10 +67,12 @@ var App = /*#__PURE__*/function (_React$Component) {
 
   _proto.render = function render() {
     if (!this.state.files) return 'Loading...';
-    return h('dl', {
+    return /*#__PURE__*/React.createElement("dl", {
       className: 'notes'
     }, this.state.files.map(function (props) {
-      return h(Post, props);
+      return /*#__PURE__*/React.createElement(Post, _extends({
+        key: props.id
+      }, props));
     }));
   };
 
@@ -92,33 +102,42 @@ var Post = /*#__PURE__*/function (_React$Component2) {
   _proto2.componentDidMount = function componentDidMount() {
     var _this4 = this;
 
-    fetch('https://www.googleapis.com/drive/v3/files/' + this.props.id + '/export?mimeType=text/html&key=AIzaSyCzDr38ArpkrukqI0gAN-rAYvbTCmEVyJ8').then(function (r) {
+    fetch("https://www.googleapis.com/drive/v3/files/" + this.props.id + "/export?mimeType=text/html&key=" + apikey).then(function (r) {
       return r.text();
     }).then(function (v) {
       return _this4.setState({
         html: trimHtml(v)
       });
     });
+    fetch("https://www.googleapis.com/drive/v2/files/" + this.props.id + "?key=" + apikey).then(function (r) {
+      return r.json();
+    }).then(function (v) {
+      return _this4.setState({
+        info: v
+      });
+    });
   };
 
   _proto2.render = function render() {
-    var date = this.props.name.slice(0, 10);
-    var name = this.props.name.slice(10);
-    var textProps = {
-      dangerouslySetInnerHTML: {
-        __html: this.state.html
-      }
-    };
-    if (!this.state.html) textProps = {
-      children: 'Loading...'
-    };
-    return h(React.Fragment, null, h('dt', null, h('strong', null, name), h('a', {
-      className: 'link-post',
+    var info = this.state.info;
+    var created = info ? niceDate2(info.createdDate) : this.props.name.slice(0, 10);
+    var modified = info ? niceDate2(info.modifiedDate) : this.props.name.slice(0, 10);
+    var name = this.props.name.slice(10) || 'Untitled ¯\\_(ツ)_/¯';
+    var textProps = {};
+    if (this.state.html) textProps.dangerouslySetInnerHTML = {
+      __html: this.state.html
+    };else textProps.children = 'Loading...';
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("dt", null, /*#__PURE__*/React.createElement("strong", null, name), /*#__PURE__*/React.createElement("a", {
+      className: 'post-link',
       href: 'https://docs.google.com/document/d/' + this.props.id + '/edit'
-    }), h('br', null), h('small', null, date)), h('dd', textProps));
+    }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("small", null, created, " ", /*#__PURE__*/React.createElement("span", {
+      style: {
+        fontSize: '0.7em'
+      }
+    }, "(edit: ", modified, ")"))), /*#__PURE__*/React.createElement("dd", textProps));
   };
 
   return Post;
 }(React.Component);
 
-ReactDOM.render(h(App, null), document.querySelector('#app'));
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.querySelector('#app'));
